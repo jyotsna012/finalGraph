77c170aa360247fadabfab912bf2c03916d38a1f
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-07 22:31:12 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,24 +2,18 @@
 
 using namespace std;
 
+  int aMatrix[20][20];
 
-void print(int aMatrix[20][20])
+void print()
 {
-    for (int a = 0; a < 20; a++)
-    {
-    for (int b = 0; b < 20; b++)
-      {
-        if(aMatrix[a][b] == NULL){
-            cout << "0" << " ";
-        }else{
+    for (int a = 0; a < 20; a++){
+        for (int b = 0; b < 20; b++){
        cout << aMatrix[a][b] << " ";
-        }
       }
       cout << endl;
     }
 }
 
 int main(){
-  int aMatrix[20][20];
-  print(aMatrix);
+  print();
 }
