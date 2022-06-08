656a6b23066b0a4e412364397f33adf280e3a6e5
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-07 22:24:24 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+
+void print(int aMatrix[][20])
+{
+    for (int a = 0; a < 20; a++)
+    {
+    for (int b = 0; b < 20; b++)
+      {
+       cout << aMatrix[a][b] << " ";
+      }
+      cout << endl;
+    }
+}
+
+int main(){
+  int aMatrix[20][20];
+  print(aMatrix);
+}
