154bd9b8a14083fc36a9cc5dff8038ee053c160e
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-06-08 19:39:49 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -104,6 +104,7 @@ void dijkstra(int start){
         for(int g = 0; g < 20; g++){
             if(aMatrix[smallDistVert - 1][g] != 0){
                 for(int w = 0; w < numVer; w++){
+                    cout << "vers: " << vers[w][0] << endl;
                     if(vers[w][0] == g){
                         index = w;
                     }
